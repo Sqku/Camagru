c53dfd676b95b087069bc195efc1314da65840e0
Alain HUA <ahua@e3r9p14.42.fr>
2016-12-22 18:29:44 +0100

    css

--- web/index.php
+++ web/index.php
@@ -9,5 +9,25 @@
     <?php
         include("header.php");
         include("menu.php");
+
+
+        ?>
+
+    <body>
+        <div class="col-6">Col6</div>
+        <div class="col-6">Col6</div>
+        <div class="col-8">Col8</div>
+        <div class="col-2">Col2</div>
+        <div class="col-2">Col2</div>
+        <div class="col-4">Col6</div>
+        <div class="col-4">Col6</div>
+        <div class="col-4">Col6</div>
+    </body>
+
+
+        <?php
+
+
+
         include("footer.php");
     ?>
